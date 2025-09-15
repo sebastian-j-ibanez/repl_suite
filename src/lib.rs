d7b0b6b02ff6246af1f7ab8fc6f4c387f47f5dfa
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2025-09-14 21:55:00 -0400

    feat: implement write and read, refactor TermManager, add error type

--- src/lib.rs
+++ src/lib.rs
@@ -1,64 +1,136 @@
 use std::{
-    io::{self, Stdin, Stdout},
+    io::{self, Read, Stdin, Stdout, Write},
     os::fd::{AsRawFd, RawFd},
+    u8,
 };
+
+/// Error type for IO and UNIX errors.
+#[derive(Debug)]
+pub enum Error {
+    Io(io::Error),
+    Errno(nix::errno::Errno),
+}
+
+impl From<io::Error> for Error {
+    fn from(e: io::Error) -> Self {
+        Error::Io(e)
+    }
+}
+
+impl From<nix::errno::Errno> for Error {
+    fn from(e: nix::errno::Errno) -> Self {
+        Error::Errno(e)
+    }
+}
 
 /// Manipulates terminal state via libc.
 pub struct TermManager {
-    pub stdin: Stdin,
-    pub stdout: Stdout,
-    pub fd: RawFd,
-    pub termios: libc::termios,
+    stdin: Stdin,
+    stdout: Stdout,
+    fd: RawFd,
+    original_termios: libc::termios,
 }
 
 impl TermManager {
-    pub fn init() -> Result<TermManager, io::Error> {
+    /// Create a new TermManager in raw mode.
+    pub fn new() -> Result<TermManager, Error> {
         let stdin = io::stdin();
         let stdout = io::stdout();
         let fd = stdin.as_raw_fd();
-        let termios = init_termios(fd)?;
+        let original_termios = enable_raw_mode(fd)?;
 
         Ok(TermManager {
             stdin,
             stdout,
             fd,
-            termios,
+            original_termios,
         })
+    }
+
+    pub fn get_stdin(&self) -> &Stdin {
+        &self.stdin
+    }
+
+    pub fn get_stout(&self) -> &Stdout {
+        &self.stdout
+    }
+
+    /// Flush stdout.
+    pub fn flush(&mut self) -> Result<(), Error> {
+        match self.stdout.flush() {
+            Ok(_) => Ok(()),
+            Err(e) => Err(Error::Io(e)),
+        }
+    }
+
+    /// Write buffer to stdout.
+    pub fn write(&mut self, data: &[u8]) -> Result<(), Error> {
+        match self.stdout.write(data) {
+            Ok(r) => {
+                if r <= 0 && data.len() <= 0 {
+                    let msg = format!("only wrote {} of {} bytes", r, data.len());
+                    return Err(Error::Io(io::Error::new(io::ErrorKind::WriteZero, msg)));
+                }
+
+                Ok(())
+            }
+            Err(e) => Err(Error::Io(e)),
+        }
+    }
+
+    /// Read byte from stdin. Return io::ErrorKind::WriteZero if no byte read.
+    pub fn read(&mut self, buf: &mut [u8; 1]) -> Result<usize, Error> {
+        match self.stdin.read(buf) {
+            Ok(0) => {
+                return Err(Error::Io(io::Error::new(
+                    io::ErrorKind::WriteZero,
+                    "read 0 bytes from stdin",
+                )));
+            }
+            Ok(bytes_read) => Ok(bytes_read),
+            Err(e) => Err(Error::Io(e)),
+        }
     }
 }
 
 impl Drop for TermManager {
-    // Restore terminal settings when TermManager is destructed.
     fn drop(&mut self) {
-        unsafe {
-            libc::tcsetattr(self.fd, libc::TCSANOW, &self.termios);
-        }
+        disable_raw_mode(self.fd, self.original_termios).unwrap();
     }
 }
 
-/// Init termios struct.
-/// Disables canonical mode and echo.
-fn init_termios(fd: RawFd) -> Result<libc::termios, io::Error> {
-    unsafe {
-        // Initialize a termios struct.
-        // See man(3) tcsetattr for more details.
-        let mut termios: libc::termios = std::mem::zeroed();
-        if libc::tcgetattr(fd, &mut termios) != 0 {
-            return Err(io::Error::last_os_error());
-        }
+/// Enable raw mode by disabling canonical mode and echo.
+fn enable_raw_mode(fd: RawFd) -> Result<libc::termios, Error> {
+    let original_termios = get_termios(fd)?;
+    let mut raw = original_termios;
+    raw.c_lflag &= !(libc::ICANON | libc::ECHO);
+    raw.c_cc[libc::VMIN] = 1;
+    raw.c_cc[libc::VTIME] = 0;
+    set_termios(fd, &raw)?;
+    Ok(original_termios)
+}
 
-        let mut raw = termios;
+/// Disable raw mode and reset terminal interface.
+fn disable_raw_mode(fd: RawFd, original_termios: libc::termios) -> Result<(), Error> {
+    set_termios(fd, &original_termios)?;
+    Ok(())
+}
 
-        // Unset ICANON and ECHO.
-        raw.c_lflag &= !(libc::ICANON | libc::ECHO);
-
-        // VMIN is the minimum number of chars to read from stdin.
-        // VTIME is the timeout for input. Disabled when 0.
-        raw.c_cc[libc::VMIN] = 1;
-        raw.c_cc[libc::VTIME] = 0;
-
-        // Apply settings.
-        libc::tcsetattr(fd, libc::TCSANOW, &raw);
-        Ok(termios)
+/// Get termios from raw file descriptors.
+fn get_termios(fd: RawFd) -> Result<libc::termios, Error> {
+    let mut termios = std::mem::MaybeUninit::uninit();
+    let res = unsafe { libc::tcgetattr(fd, termios.as_mut_ptr()) };
+    if res != 0 {
+        return Err(Error::Io(io::Error::last_os_error()));
     }
+    Ok(unsafe { termios.assume_init() })
+}
+
+/// Set termios settings from raw file descriptors.
+fn set_termios(fd: RawFd, termios: &libc::termios) -> Result<(), Error> {
+    let res = unsafe { libc::tcsetattr(fd, libc::TCSANOW, termios) };
+    if res != 0 {
+        return Err(Error::Io(io::Error::last_os_error()));
+    }
+    Ok(())
 }
